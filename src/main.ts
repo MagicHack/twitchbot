7a06424afe046103fbcbb5d3118aba7acc52b847
Émile Larose-Levac <emile.larose@gmail.com>
2021-07-08 03:40:40 -0400

    Typescript rewrite start

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import winston from "winston";
+
+import {TwitchBot} from "./twitch_bot";
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'MagicBot' },
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+logger.info("MagicBot started");
+
+const TWITCH_USERNAME = getEnvValue('TWITCH_USERNAME');
+const TWITCH_TOKEN = getEnvValue('TWITCH_TOKEN');
+
+
+
+
+function getEnvValue(key :string) :string {
+    const result = process.env[key];
+    if(result === undefined) {
+        const message = `Error reading ${key} from .env file`;
+        logger.error(message);
+        throw message;
+    }
+    return result!;
+}
