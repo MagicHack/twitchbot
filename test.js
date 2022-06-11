a1aa63e3b5cf2202985427f95d092c1c4249b04e
Émile Larose-Levac <emile.larose@gmail.com>
2022-06-11 14:02:29 -0400

    add missing dot

--- test.js
+++ test.js
@@ -374,7 +374,7 @@ client.on('message', (channel, tags, message, self) => {
         } else if (isCommand(cleanMessage.toLowerCase(), "lastraid")) {
             if (lastRaid !== null) {
                 let timeSinceRaidSeconds = (Date.now() - new Date(lastRaid["ts"])) / 1000;
-                let status = "in progress.";
+                let status = "in progress";
                 if(lastRaid["won"] !== undefined) {
                     if(lastRaid["won"]) {
                         status = "won";
@@ -382,7 +382,7 @@ client.on('message', (channel, tags, message, self) => {
                         status = "lost";
                     }
                 }
-                sendMessage(channel, "Last raid " + prettySeconds(timeSinceRaidSeconds) + " ago. Status: " + status + " (level " + lastRaid["level"] + ")");
+                sendMessage(channel, "Last raid " + prettySeconds(timeSinceRaidSeconds) + " ago. Status: " + status + ". (level " + lastRaid["level"] + ")");
             } else {
                 sendMessage(channel, "No raids recorded yet");
             }
